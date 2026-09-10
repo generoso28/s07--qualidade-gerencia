0933b4305b16fedc67137b1c857bd6aebe52d09f
generoso28 <lucas.caixeta10@gmail.com>
2026-09-10 00:16:25 -0300

    Adição do teste TC-002: Login com usuário com delay

--- tests/auth.spec.ts
+++ tests/auth.spec.ts
@@ -22,4 +22,22 @@ test.describe('Autenticação e Sessão', () => {
     await expect(inventoryPage.title).toBeVisible();
     await expect(inventoryPage.title).toHaveText(testData.headers.productsTitle);
   });
+  test('TC-002: Login com usuário com delay', async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const inventoryPage = new InventoryPage(page);
+
+    // Pré-condição: Usuário na tela de login
+    await loginPage.goto();
+
+    // Ação: Preencher usuário e senha e clicar em Login
+    await loginPage.login(
+      testData.users.performanceGlitch.username,
+      testData.users.performanceGlitch.password
+    );
+
+    // Resultado esperado: Redireciona para "/inventory.html" e título "Products" é visível
+    await expect(page).toHaveURL(/.*inventory\.html/);
+    await expect(inventoryPage.title).toBeVisible();
+    await expect(inventoryPage.title).toHaveText(testData.headers.productsTitle);
+  });
 });
